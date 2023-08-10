d52fd35974acc7cea1c88f6459ee3b1d3dc9e179
“MakawaMT” <mtmakawa@gmail.com>
2023-08-10 12:05:56 +0200

    * mapped array to list items * commented out static list items * added array of town/cities in Malawi * updated version number

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,5 +1,27 @@
-function ListGroup(){
-    return <h1>List Group</h1>;
+import { Fragment } from "react";
+function ListGroup() {
+  const items = ["Lilongwe", "Blantyre", "Zomba", "Rumphi", "Nkhata Bay"];
+
+  /* 
+<li className="list-group-item">An item</li>
+        <li className="list-group-item">A second item</li>
+        <li className="list-group-item">A third item</li>
+        <li className="list-group-item">A fourth item</li>
+        <li className="list-group-item">And a fifth one</li>
+        <li className="list-group-item active" aria-current="true">
+          An active item
+        </li>
+*/
+  return (
+    <>
+      <h1>List</h1>
+      <ul className="list-group">
+        {items.map((item) => (
+          <li>{item}</li>
+        ))}
+      </ul>
+    </>
+  );
 }
 
 export default ListGroup;
