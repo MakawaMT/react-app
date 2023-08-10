99d5e30337d587d0fc128bc4e18fb9c95f65397c
“MakawaMT” <mtmakawa@gmail.com>
2023-08-10 14:27:43 +0200

    * added visibility state for button click/show * added onClick event for closing button * added prop for onClose click

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,18 @@
+import { useState } from "react";
 import Message from "./Message";
 import Alert from "./components/Alert";
 import Button from "./components/Button";
 import ListGroup from "./components/ListGroup";
 
 function App() {
-  let items = ["Lilongwe", "Blantyre", "Zomba", "Rumphi", "Nkhata Bay"];
-  let heading = "Cities (MW)";
-
-  const handleSelectItem = (item: string) => {
-    console.log(item);
-  };
-
-  const handleOnClick = () => {
-    return <Alert children="Show Up"></Alert>;
-  };
+  const [alertVisible, setAlertVisibility] = useState(false);
 
   return (
     <div>
-      <Button color="danger" onClick={() => console.log("Clicked")}>
+      {alertVisible && (
+        <Alert onClose={() => setAlertVisibility(false)}>My Alert</Alert>
+      )}
+      <Button color="primary" onClick={() => setAlertVisibility(true)}>
         World
       </Button>
     </div>
